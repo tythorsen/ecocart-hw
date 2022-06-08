983e2701d579a80d5159e2df69b5ebeee36d7522
Ty Thorsen <thorsenty@gmail.com>
2022-06-08 13:22:10 -0500

    style page and search bar

--- src/App.svelte
+++ src/App.svelte
@@ -25,22 +25,29 @@
   };
 </script>
 
-<form on:submit={search}>
-  <input type="text" on:change={queryChange}/>
-  <button type="submit">Search</button>
-</form>
-
-{#if artistPromise}
-  {#await artistPromise}
-    <p>Searching...</p>
-  {:then artist}
-    {#if artist}
-      <ArtistHero {artist}/>
-    {:else}
-      <p>Artist "{lastQuery}" not found</p>
-    {/if}
-  {/await}
-{/if}
+<div class="toolbar">
+  <form on:submit={search}>
+    <input type="text" placeholder="Search for artists" on:change={queryChange}/>
+    <button type="submit">Search</button>
+  </form>
+</div>
+<div class="view">
+  {#if artistPromise}
+    {#await artistPromise}
+    <div class="searching">
+      <p>Searching for "{lastQuery}"</p>
+    </div>
+    {:then artist}
+      {#if artist}
+        <ArtistHero {artist}/>
+      {:else}
+        <div class="error">
+          <p>Artist "{lastQuery}" not found</p>
+        </div>
+      {/if}
+    {/await}
+  {/if}
+</div>
 
 <style>
   :root {
@@ -48,5 +55,70 @@
     font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
     font-size: 16px;
     line-height: 1.25;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  .toolbar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 72px;
+    width: 100vw;
+    display: flex;
+    align-content: center;
+    align-items: center;
+    justify-content: center;
+    background: #ffffff;
+    padding: 0 12px;
+    border-bottom: 1px solid #cdcdcd;
+  }
+
+  form {
+    width: 100%;
+    max-width: 546px;
+    border: 1px solid #cdcdcd;
+    border-radius: 2px;
+    display: flex;
+    overflow: hidden;
+  }
+
+  input {
+    flex: 1;
+    border: none;
+    font-size: 16px;
+    line-height: 36px;
+    height: 46px;
+    padding: 0 24px;
+  }
+
+  button {
+    cursor: pointer;
+    background-color: #00b4b3;
+    color: #ffffff;
+    font-size: 16px;
+    padding: 0 24px;
+    border: none;
+  }
+
+  .view {
+    padding: 120px 12px 48px;
+  }
+
+  .searching, .error {
+    background: #ffffff;
+    text-align: center;
+    max-width: 546px;
+    padding: 12px 24px;
+    border: 1px solid #cdcdcd;
+    border-radius: 2px;
+    margin: 0 auto;
+  }
+
+  .error {
+    color: red;
+    border-color: red;
   }
 </style>
