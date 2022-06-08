c44db8ccc1ab890e0ee263572a26cb5546d2f517
Ty Thorsen <thorsenty@gmail.com>
2022-06-08 11:08:27 -0500

    wire up basic search

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,44 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  let artistPromise = null;
+  let lastQuery = "";
+  let numberFormatter = new Intl.NumberFormat('en-US');
+  let query = "";
+
+  async function getArtist() {
+    const artistUrl = `https://rest.bandsintown.com/artists/${encodeURIComponent(query)}?app_id=test`;
+    const resp = await fetch(artistUrl);
+    const artist = await resp.json();
+
+    return artist;
+  }
+
+  function queryChange(ev) {
+    query = ev.target.value;
+  };
+
+  function search(ev) {
+    ev.preventDefault();
+
+    lastQuery = query;
+    artistPromise = getArtist();
+  };
 </script>
 
-<main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
+<form on:submit={search}>
+  <input type="text" on:change={queryChange}/>
+  <button type="submit">Search</button>
+</form>
 
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
-</main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
-</style>
+{#if artistPromise}
+  {#await artistPromise}
+    <p>Searching...</p>
+  {:then artist}
+    {#if artist}
+      <h1>{artist.name}</h1>
+      <p>{numberFormatter.format(artist.tracker_count)} Followers</p>
+      <p>{numberFormatter.format(artist.upcoming_event_count)} Upcoming {artist.upcoming_event_count == 1 ? "Concert" : "Concerts"}</p>
+    {:else}
+      <p>Artist "{lastQuery}" not found</p>
+    {/if}
+  {/await}
+{/if}
